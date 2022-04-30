84093db04e04e667a91b89e33b29157ae119a93b
luisgcodes <lcienfuegos95@gmail.com>
2022-04-29 19:26:41 -0700

    hamburger menu

--- develop/Script/script.js
+++ develop/Script/script.js
@@ -1,7 +1,37 @@
-const button = document.querySelector('#menu-button');
-const menu = document.querySelector('#menu');
+// const button = document.querySelector('#menu-button');
+// const menu = document.querySelector('#menu');
+const hamburgerMenu = document.querySelector("#hamburger-menu");
 
 
-button.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
+// button.addEventListener('click', () => {
+//   menu.classList.toggle('hidden');
+// });
+
+
+function toggleNav() {
+  // Toggle: Hamburger Open/Close
+  hamburgerMenu.classList.toggle("active");
+
+  //   Toggle: Menu Active
+  overlay.classList.toggle("overlay-active");
+
+  if (overlay.classList.contains("overlay-active")) {
+    // Animate In - Overlay
+    overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+
+    // Animate In - Nav Items
+    navAnimation("out", "in");
+  } else {
+    // Animate Out - Overlay
+    overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+
+    // Animate Out - Nav Items
+    navAnimation("in", "out");
+  }
+}
+
+// Events Listeners
+hamburgerMenu.addEventListener("click", toggleNav);
+navItems.forEach((nav) => {
+  nav.addEventListener("click", toggleNav);
 });
